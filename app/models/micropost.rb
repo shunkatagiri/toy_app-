af2be3a2719c7d73a70db9e073edc607e2acf736
shunkatagiri <shun.katagiri1214@gmail.com>
2023-08-06 21:45:30 +0900

    ユーザーとマイクロポストの関連付けとバリデーションの追加
    - Userモデルにhas_many :micropostsの関連付けを追加
    - Micropostモデルにbelongs_to :userの関連付けを追加
    - Micropostのcontentに140文字制限のバリデーションを設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
-vclass Micropost < ApplicationRecord
-validates :content, length: { maximum: 140 }
+class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content, length: { maximum: 140 }
 end
